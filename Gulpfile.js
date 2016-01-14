8eeae5fd0f0c780d282d3d5eea81e94510a1ad43
dmitriz <dmitri14@gmail.com>
2016-01-14 15:40:02 +0000

    Add webpack-stream

--- Gulpfile.js
+++ Gulpfile.js
@@ -1,9 +1,16 @@
 // Gulpfile.js
 var gulp = require('gulp');
+var webpack = require('webpack-stream');
 
 gulp.task('default', ['html']);
 
 gulp.task('html', function(){
-	gulp.src('src/index.html')
-			.pipe(gulp.dest('build'));
+	return gulp.src('src/index.html')
+		.pipe(gulp.dest('build'));
+});
+
+gulp.task('webpack', function() {
+  return gulp.src('src/main.js')
+    .pipe(webpack())
+    .pipe(gulp.dest('build/'));
 });
