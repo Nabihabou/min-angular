91be8cafff07b3e87a76249a47844eb665886cda
Dmitri Zaitsev <dmitri14@gmail.com>
2016-01-24 00:51:59 +0000

    Move to demo

--- index.js
+++ index.js
@@ -1,26 +1,21 @@
 // index.js
-/* globals cp */
 
 /*
  * Script to run after npm install
  *
- * Currently: copying selected files to user's directory
+ * Copy selected files to user's directory
  */
 
-// import shell tools for global access
-// https://github.com/shelljs/shelljs#javascript
-require('shelljs/global')
+'use strict'
 
+var gentlyCopy = require('gently-copy')
+
+var filesToCopy = 'demo'
+
+// User's local directory
+// Warning: This assumes the package is installed into `node_modules/<package-name>/`
 // TODO: Find a more robust solution
-// Current user directory
 var userPath = '../../'
 
-// Moving files to the local directory
-var filesToCopy = ['index.html']
-
-filesToCopy.forEach(function (file) {
-  console.log('Copying file or directory: ', file)
-
-  // cp is defined through shelljs
-  cp('-R', file, userPath)
-})
+// Moving files to user's local directory
+gentlyCopy(filesToCopy, userPath)
